fb77625c9994e2307657ca13d79124787830ae4d
hns140412-glitch <hns140412@gmail.com>
2026-09-21 18:44:44 +0900

    reconcile(v2): port snap-release-v01.js

---
+++ snap-release-v01.js
@@ -0,0 +1,13 @@
+(function(root){
+  'use strict';
+  root.SnapPopReleaseDescriptor=Object.freeze({
+    app_id:'snap-pop',
+    app_version:'REV_12',
+    runtime_version:'snap-runtime-rev12',
+    data_schema_version:1,
+    contract_version:1,
+    release_id:'snap-pop-rev12-r1',
+    master_revision:'REV_12',
+    shared_runtime_basis:'TAKY@d6dd9b19490918efe5063bf719e2b2b8cc1db2b9'
+  });
+})(typeof globalThis!=='undefined'?globalThis:this);
